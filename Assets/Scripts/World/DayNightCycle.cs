c118d0b07311f0b1b25f13ce5e26106b0aa0328e
balu <me@balu.moe>
2025-10-14 17:32:34 +0200

    Added a day-night cycle
    spent too much time on this to figure out how it works

---
+++ Assets/Scripts/World/DayNightCycle.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField] private Light sunLight;
+
+    [Header("Day-Night cycle settings")] 
+    [SerializeField] private float startTime;
+
+    [Tooltip("In Minutes")] 
+    [SerializeField] private float cycleDuration;
+
+    [Header("Lighting settings")] 
+    [SerializeField] private float lightIntensityNight = 0f;
+    [SerializeField] private float lightIntensityDay = 1f;
+    
+    [SerializeField] private float sunriseAngle = -90f;
+    [SerializeField] private float sunsetAngle = 90f;
+
+    private float currentTimeOfDay;
+    private float timeOfDayNormalized;
+
+    private void Start()
+    {
+        // Convert day start time from minutes to seconds
+        currentTimeOfDay = startTime * 60;
+    }
+
+    public void Update()
+    {
+        currentTimeOfDay += Time.deltaTime;
+        
+        float cycleDurationSeconds = cycleDuration * 60;
+        
+        timeOfDayNormalized = (currentTimeOfDay % cycleDurationSeconds) / cycleDurationSeconds;
+        
+        float sunAngle = timeOfDayNormalized * 360f;
+        sunLight.transform.localRotation = Quaternion.Euler(sunAngle, 0f, 0f);
+        
+        float intensityFactor = Mathf.Clamp01(-Mathf.Cos(timeOfDayNormalized * Mathf.PI * 2f));
+        sunLight.intensity = Mathf.Lerp(lightIntensityNight, lightIntensityDay, intensityFactor);
+    }
+}
